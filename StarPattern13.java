1547e187acaa843a91a025927416714477764fbd
Kamal chandra joshi <kj144049@gmail.com>
2024-09-13 21:28:58 +0530

    This program will print a star pattern

---
+++ StarPattern13.java
@@ -0,0 +1,42 @@
+package JavaProgram;
+import java.util.Scanner;
+// Program no :- 28;
+/*
+ *  *  *  *  *
+ *           *
+ *           *
+ *           *
+ *  *  *  *  *
+ */
+public class StarPattern13 {
+    public static void pattern(int n){
+        for(int i=0; i<=(n-1); i++){
+            for(int j=0; j<=(n-1); j++){
+                if(i==0 || i==(n-1)){
+                    System.out.print("  *");
+                }
+                else{
+                    if(j==0 || j==(n-1)){
+                        System.out.print("  *");
+                    }
+                    else{
+                        System.out.print("   ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char choice = 'y';
+        int no_of_lines;
+        while(choice != 'n'){
+            System.out.print("Enter the no of lines :- ");
+            no_of_lines = sc.nextInt();
+            pattern(no_of_lines);
+            System.out.print("Do you want to continue if yes(type anything) or no(type n) :- ");
+            choice = sc.next().charAt(0);
+        }
+    }
+}
