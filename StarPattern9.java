ec3689942f8acca5f67dffa46e6b4a2b57c60e3d
Kamal chandra joshi <kj144049@gmail.com>
2024-09-13 18:08:17 +0530

    this program will print the star pattern

---
+++ StarPattern9.java
@@ -0,0 +1,37 @@
+package JavaProgram;
+import java.util.Scanner;
+// Program no :- 24;
+/*
+    *
+      *
+        *
+          *
+            *
+ */
+public class StarPattern9 {
+    public static void pattern(int n){
+        for(int i=0; i<=(n-1); i++){
+            for(int j=0; j<=(n-1); j++){
+                if(i==j){
+                    System.out.print("  *");
+                }
+                else{
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char choice = 'y';
+        int no_of_lines;
+        while(choice != 'n'){
+            System.out.print("Enter the number of lines :- ");
+            no_of_lines = sc.nextInt();
+            pattern(no_of_lines);
+            System.out.print("Do you want to continue if yes(type anything) or no(type n) :- ");
+            choice = sc.next().charAt(0);
+        }
+    }
+}
